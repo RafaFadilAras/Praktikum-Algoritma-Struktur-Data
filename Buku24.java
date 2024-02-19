b64f5842c8e8b87178204033a7df865352c782e7
rfclz <rafafadilaras@gmail.com>
2024-02-19 19:20:41 +0700

    add percobaan1

--- Buku24.java
+++ Buku24.java
@@ -4,7 +4,7 @@ public class Buku24 {
     int halaman, stok, harga;
     
     void tampilInformasi() {
-        System.out.println("Judul : " +judul);
+        System.out.println("Judul: " +judul);
         System.out.println("Pengarang : " +pengarang);
         System.out.println("Jumlah halaman : " +halaman);
         System.out.println("Sisa stok : " +stok);
