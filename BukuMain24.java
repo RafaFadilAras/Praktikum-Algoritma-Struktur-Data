5b7866cf3546bd6aa0d12c40d8883ba4bfc98c4b
rfclz <rafafadilaras@gmail.com>
2024-02-21 21:20:42 +0700

    add percobaan3 konstruktor main

--- BukuMain24.java
+++ BukuMain24.java
@@ -1,9 +1,6 @@
 public class BukuMain24 {
     public static void main(String[] args) {
 
-        String judul, pengarang;
-        int halaman, stok, harga, jml;
-
         Buku24 bk1 = new Buku24();
 
         bk1.judul = "Today Ends Tomorrow Comes";
@@ -13,9 +10,13 @@ public class BukuMain24 {
         bk1.harga = 71000;
 
         bk1.tampilInformasi();
-        bk1.terjual(jml=5);
-        bk1. gantiHarga(harga=60000);
+        bk1.terjual(5);
+        bk1. gantiHarga(60000);
         bk1.tampilInformasi();
+
+        Buku24 bk2 = new Buku24("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
 
     }
 }
