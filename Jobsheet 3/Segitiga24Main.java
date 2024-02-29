06e9c7eb3c5ecb0cd8162396602213e71acdd649
rfclz <rafafadilaras@gmail.com>
2024-02-29 07:40:38 +0700

    add array segitiga

---
+++ Jobsheet 3/Segitiga24Main.java
@@ -0,0 +1,11 @@
+public class Segitiga24Main {
+    public static void main(String[] args) {
+        Segitiga24[] sgArray = new Segitiga24[4];
+
+        sgArray[0] = new Segitiga24(10, 4);
+        sgArray[1] = new Segitiga24(20, 10);
+        sgArray[2] = new Segitiga24(15, 6);
+        sgArray[3] = new Segitiga24(25, 10);
+
+    }
+}
