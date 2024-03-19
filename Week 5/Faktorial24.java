4bd4d30983bee17ebeab2936953588d1efd80961
rfclz <rafafadilaras@gmail.com>
2024-03-19 12:25:13 +0700

    add modify dowhile percobaan1

--- Week 5/Faktorial24.java
+++ Week 5/Faktorial24.java
@@ -3,10 +3,15 @@ public class Faktorial24 {
 
     public int faktorialBF (int n) {
         int fakto = 1;
-        for (int i=1; i <= n; i++) {
-            fakto = fakto * i;
-        }
-        return fakto;
+        int i = 1;
+        do {
+            fakto *= i;
+            i++;
+        } while (i<=n);
+            return fakto;
+        //for (int i=1; i <= n; i++) {
+            //fakto = fakto * i;
+        //}
     }
 
     public int faktorialDC(int n) {
