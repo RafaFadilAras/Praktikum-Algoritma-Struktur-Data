c02f2f4916312bf5571cb7b132cac0e15839fd42
rfclz <rafafadilaras@gmail.com>
2024-03-25 10:34:10 +0700

    add latihan jobsheet6 hotel

---
+++ Week 6/packageMinggu6/Hotel24.java
@@ -0,0 +1,20 @@
+public class Hotel24 {
+    String nama, kota;
+    int harga;
+    byte bintang;
+    
+    Hotel24(String n, String k, int h, byte i) {
+        nama = n;
+        kota = k;
+        harga = h;
+        bintang = i;
+    }
+    
+    void tampil() {
+        System.out.println("Nama    : " +nama);
+        System.out.println("Kota    : " +kota);
+        System.out.println("Harga   : " +harga);
+        System.out.println("Rate    : " +bintang);
+    }
+
+}
