93c030afbc958222aae81b15264ff4de382c24b4
Rafa Fadil Aras <rafafadilaras@gmail.com>
2024-04-05 21:18:06 +0700

    add percobaan 1 stack

---
+++ Week 8/Barang24.java
@@ -0,0 +1,11 @@
+public class Barang24 {
+    int kode;
+    String nama, kategori;
+
+    public Barang24 (int kd, String nm, String ktg) {
+        kode = kd;
+        nama = nm;
+        kategori = ktg;
+    }
+    
+}
