48fbc4f5b948ed26802c34562c02d143477125fa
Rafa Fadil Aras <rafafadilaras@gmail.com>
2024-04-29 10:08:10 +0700

    add praktikum 2 week 10

---
+++ Week 10/Nasabah24.java
@@ -0,0 +1,17 @@
+public class Nasabah24 {
+    String norek, nama, alamat;
+    int umur;
+    double saldo;
+
+    Nasabah24 (String norek, String nama, String alamat, int umur, double saldo) {
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }
+
+    Nasabah24() {
+        
+    }
+}
---
+++ Week 10/Nasabah24Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Nasabah24Main {
+    public static void menu() {
+        System.out.println("Pilih Menu: ");
+        System.out.println("1. Antrian Baru");
+        System.out.println("2. Antrian Keluar");
+        System.out.println("3. Cek Antrian Terdepan");
+        System.out.println("4. Cek Semua Antrian");
+        System.out.println("5. Cek Antrian Paling Belakang");
+        System.out.println("----------------------------------------");
+    }
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.print("Masukkan kapasitas queue: ");
+            int jumlah = sc.nextInt();
+            
+            Queue24 antri = new Queue24(jumlah);
+
+            int pilih;
+    
+            do {
+                menu();
+                pilih = sc.nextInt();
+                sc.nextLine();
+                switch (pilih) {
+                    case 1:
+                        System.out.print("No Rekening: ");
+                        String norek =sc.nextLine();
+                        System.out.print("Nama: ");
+                        String nama =sc.nextLine();
+                        System.out.print("Alamat: ");
+                        String alamat =sc.nextLine();
+                        System.out.print("Umur: ");
+                        int umur =sc.nextInt();
+                        System.out.print("Saldo: ");
+                        double saldo =sc.nextDouble();
+                        Nasabah24 nb = new Nasabah24(norek, nama, alamat, umur, saldo);
+                        sc.nextLine();
+                        antri.Enqueue(nb);
+                        break;
+                    case 2:
+                        Nasabah24 data = antri.Dequeue();
+                        if (!"".equals(data.norek) && !"".equals(data.nama) && !"".equals(data.alamat) && data.umur != 0 && data.saldo != 0) {
+                            System.out.println("Antrian yang keluar: " + data.norek + " " + data.nama + " " + data.alamat + " " + data.umur + " " + data.saldo);
+                            break;
+                        }
+                    case 3:
+                        antri.peek();
+                        break;
+                    case 4:
+                        antri.print();
+                        break;
+                    case 5 :
+                        antri.peekRear();
+                        break;
+                }  
+            } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4);
+        }
+}
+
